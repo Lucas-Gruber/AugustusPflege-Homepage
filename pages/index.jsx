67a1b58e85175e88b640de82d97b5ccc65dacd10
Thomas-Blakeney <blaumc@icloud.com>
2021-12-07 19:25:32 +0100

    initial commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 export default function Home() {
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -8,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
@@ -64,7 +65,7 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
+      </main> 
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
