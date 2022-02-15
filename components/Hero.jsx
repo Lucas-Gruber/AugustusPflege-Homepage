3824da01c8ced61cc3698657dcdef42a23f50a0c
Thomas Blakeney <72545889+Thomas-Blakeney@users.noreply.github.com>
2022-02-15 13:49:43 +0100

    fix: fixed the heading in the hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -34,10 +34,10 @@ export default function Hero() {
           <main className="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Beispiel</span>{" "}
+                <span className="block xl:inline">Augustus</span>{" "}
                 <span className="block text-green-600 xl:inline">
                   <br />
-                  Überschrift
+                  Pflege
                 </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
